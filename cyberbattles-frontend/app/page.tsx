a625284fbcb5063531a7aadf3e73e7a6cc35d212
meemum <liamdjasher@icloud.com>
2025-08-14 21:30:02 +1000

    Add rough login page

--- cyberbattles-frontend/app/page.tsx
+++ cyberbattles-frontend/app/page.tsx
@@ -1,5 +1,7 @@
 import logo from "../public/images/logo.png"
+import cyberbattles from "../public/images/cyberbattles.png"
 import Image from 'next/image'
+import React from "react";
 export default function LoginPage() {
   return (
     <div className="p-10">
@@ -8,7 +10,57 @@ export default function LoginPage() {
         <Image src={logo} alt="logo of the website"/>     
       </div>
       
+<section className="min-h-screen flex items-center justify-start pl-150">
+  <div className="flex relative gap-8">
+
+    {/* Left: Form */}
+    <form action="" className="flex flex-col gap-4 relative">
+      {/* Input container */}
+      <div className="relative flex flex-col gap-4">
+        <input
+          className="p-7 rounded-xl border w-100"
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <input
+          className="p-7 rounded-xl border w-100"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+
+        {/* Image positioned at the vertical midpoint */}
+        <div className="absolute left-full ml-4 top-1/2 -translate-y-1/2 w-100 h-100">
+          <Image
+            src={cyberbattles}
+            alt="text based logo of the website"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      </div>
+
+      {/* Buttons */}
+      <div className="flex justify-between mt-4">
+        <p className="self-center cursor-pointer">Create Account</p>
+        <button className="outline-solid rounded-xl text-white py-2 px-6 hover:scale-105 duration-300">
+          Login
+        </button>
+      </div>
+    </form>
+
+  </div>
+</section>
+
+
+
+          
+
+        
     </div>
+
     
   );
 }
+
