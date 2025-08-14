0021b36e25bee5c1c80e97216fb4e08034380e10
meemum <liamdjasher@icloud.com>
2025-08-14 19:15:16 +1000

    set up tailwind

--- cyberbattles-frontend/app/page.tsx
+++ cyberbattles-frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function LoginPage() {
   return (
     <div className="p-10">
-
+      
       <div className="absolute top-4 left-4 w-25 h-25">
         <Image src={logo} alt="logo of the website"/>     
       </div>
