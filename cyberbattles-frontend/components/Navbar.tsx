669e3e89e458990013937a0b4eac83121c8ea2cb
Noah Beasley <noahbz@live.com.au>
2025-09-02 00:43:10 +1000

    Profile editing (pfp & uname) functionality added

--- cyberbattles-frontend/components/Navbar.tsx
+++ cyberbattles-frontend/components/Navbar.tsx
@@ -6,69 +6,77 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { auth } from "@/lib/firebase";
 
 function Navbar() {
-    const genericItems = ["Home", "Lab", "Learn"]
-    const genericLinks = ["/", "/lab", "/learn"]
-    const userItems = ["Home", "Leaderboard", "Traffic", "Shell"]
-    const userLinks = ["/", "/", "/", "/", "/"]
+  const genericItems = ["Home", "Lab", "Learn"]
+  const genericLinks = ["/", "/lab", "/learn"]
+  const userItems = ["Home", "Leaderboard", "Traffic", "Shell"]
+  const userLinks = ["/", "/", "/", "/", "/"]
 
-    const [[loggedIn, dname, items, links], setLoggedIn] = useState([false, "", genericItems, genericLinks]);
+  const [[items, links], setItems] = useState([genericItems, genericLinks]);
+  const [currentUser, setCurrentUser] = useState<any | null>(null)
+  const [photoURL, setPhotoURL] = useState("https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png")
 
-    {/* Check if the user auth state has changed. If so update the navbar .*/}
-    try{
-        onAuthStateChanged(auth, (user) => {
-            if (user && !loggedIn){
-                let dispName = ""
-                if (user.displayName){
-                    dispName = user.displayName
-                }
-                setLoggedIn([true, dispName, userItems, userLinks])
+  {/* Check if the user auth state has changed. If so update the currentUser .*/}
+  try{
+    onAuthStateChanged(auth, (user) => {
+        if (user && !currentUser){
+            setCurrentUser(user)
+            if (user.photoURL){
+                setPhotoURL(user.photoURL)
             }
-        })
-    }  catch (error) {
-        setLoggedIn([false, "", genericItems, genericLinks])
-        console.error("Navbar failed:", error);
-    }
+            setItems([userItems, userLinks])
+        }
+    })
+  }  catch (error) {
+    setCurrentUser(null)
+    console.error("Failed:", error);
+  }
     
-    return (
-        <nav className="fixed w-full h-24 shadow-xl bg-black z-50">
-          <div className="flex justify-between items-center h-full w-full px-4">
-            <div className="flex justify-between items-center">
-              <Image src={logo} alt="logo" width={150} className="ml-5 mr-5" />
-              <ul className="hidden sm:flex">
-                {items.map((item, index) => (
-                  <Link key={item} href={links[index]}>
-                    <li className="ml-20 lowercase text-2xl hover:scale-105 duration-300 font-bold cursor-pointer">
-                      {item}
-                    </li>
-                  </Link>
-                ))}
-              </ul>
-            </div>
-            <div className="flex justify-between items-center">
-            <ul className="hidden md:flex">
-                    { loggedIn &&
-                    <Link href="/dashboard">
-                    <li className="mr-20 text-2xl hover:scale-110 duration-300">
-                        {dname}
-                    </li>
-                    </Link>
-                    }
-                    { !loggedIn &&
-                    <Link href="/login">
-                    <li className="mr-20 text-2xl hover:scale-110 duration-300">
-                        Login
-                    </li>
-                    </Link>
-                    }
-                </ul>
-                { loggedIn &&
-                <div className="w-20 h-20 bg-gray-300 rounded-full ml-5 mr-10"></div>
+  return (
+    <nav className="fixed w-full h-40 shadow-xl bg-black z-50">
+      <div className="flex justify-between items-center h-full w-full px-4">
+        <div className="flex justify-between items-center">
+          <Image src={logo} alt="logo" width={150} className="ml-5 mr-5" />
+          <ul className="hidden md:flex">
+            {items.map((item, index) => (
+              <Link key={item} href={links[index]}>
+                <li className="ml-20 lowercase text-2xl hover:scale-105 duration-300 font-bold cursor-pointer">
+                  {item}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div className="flex justify-between items-center">
+        <ul className="hidden md:flex">
+                { currentUser &&
+                <Link href="/dashboard">
+                <li className="mr-20 text-2xl hover:scale-110 duration-300">
+                    {currentUser.displayName}
+                </li>
+                </Link>
                 }
+                { !currentUser &&
+                <Link href="/login">
+                <li className="mr-20 text-2xl hover:scale-110 duration-300">
+                    Login
+                </li>
+                </Link>
+                }
+            </ul>
+            { currentUser &&
+            <Link href="/profile">
+            <img
+              src={photoURL}
+              alt="avatar"
+              width={100}
+              className="rounded-full mr-10" />
+            </Link>
+            }
 
-            </div>
-          </div>
-        </nav>
-      );
-    }
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar
