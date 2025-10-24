8ef0ca9178fa992102122967cb9dc810923d3f89
smp46 <me@smp46.me>
2025-10-24 15:34:23 +1000

    fix: apis and admin end game

--- cyberbattles-frontend/components/ApiClient.tsx
+++ cyberbattles-frontend/components/ApiClient.tsx
@@ -1,25 +1,15 @@
 import axios from 'axios';
 
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:1337/api';
+
+export const WEBSOCKET_URL =
+  process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://localhost:1337';
+
 const ApiClient = axios.create({
-  baseURL: 'https://cyberbattl.es/api',
+  baseURL: BACKEND_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 export default ApiClient;
-
-
-
-
-
-// If you are running the backend on localhost - use this instead:
-
-// import axios from 'axios';
-
-// const ApiClient = axios.create({
-//   baseURL: 'http://localhost:1337/api',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-// export default ApiClient;
